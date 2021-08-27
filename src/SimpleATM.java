8956a810ff3c65900087fae49d3aefe086327246
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-08-27 15:02:26 -0700

    Test

--- src/SimpleATM.java
+++ src/SimpleATM.java
@@ -1,4 +1,13 @@
+import java.util.*;
 
-public class SimpleATM {
-
+public class SimpleATM
+{
+	ArrayList bank = new ArrayList (String);
+	String id;
+	double amount;
+	
+	public void deposit (String id, double amount)
+	{
+		
+	}
 }
