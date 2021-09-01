8acd85b6c01e59f4450a0fec99947de4b8bbe480
ShizumeruMaking <87250151+ShizumeruMaking@users.noreply.github.com>
2021-09-01 12:49:06 -0700

    revised deposit, added withdraw and checkBalance

--- src/SimpleATM.java
+++ src/SimpleATM.java
@@ -1,16 +1,43 @@
 import java.util.*;
-
 public class SimpleATM
 {
 	private HashMap <String, Double> map = new HashMap <> ();
-	
+	public double checkBalance(String id)
+	{
+		if(map.containsKey(id)) {
+			return map.get(id);
+		}
+		else
+		{
+			throw new IndexOutOfBoundsException("No such ID exists");
+		}
+	}
+	public void withdraw(String id,double amount)
+	{
+		if (map.containsKey(id))
+		{
+			if(map.get(id)<amount)
+			{
+				System.out.println("please don't steal money from us");
+			}
+			else {
+			double oldValue = map.remove(id);
+			double newValue = oldValue - amount;
+			map.put(id, newValue);
+			}
+		}
+		else
+		{
+			System.out.println("no such id exists");
+		}
+	}
 	public void deposit (String id, double amount)
 	{
 		if (amount > 0)
 		{
 			if (map.containsKey(id))
 			{
-				double oldValue = map.get(id);
+				double oldValue = map.remove(id);
 				double newValue = oldValue + amount;
 				map.put(id, newValue);
 			}
