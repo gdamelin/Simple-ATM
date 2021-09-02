5f4817cacd46474399e18a5291cc95cb2c47ff8d
DidSomeoneSayBoba <89625819+DidSomeoneSayBoba@users.noreply.github.com>
2021-09-02 14:09:10 -0700

    pull req, changes compiler and has changes requested in assignment (#1)
    * revised deposit, added withdraw and checkBalance
    
    * modified withdraw to use error instead of print message
    
    * changed var name
    
    Co-authored-by: ShizumeruMaking <87250151+ShizumeruMaking@users.noreply.github.com>

---
+++ src/ATMTester.java
@@ -0,0 +1,19 @@
+
+public class ATMTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleATM atm = new SimpleATM();
+		atm.deposit("Winfrey", 25159684372.43);
+		atm.deposit("Bezos", 193495330293.43);
+		atm.deposit("Bezos", 20394353.35);
+		atm.withdraw("Bezos", 3593293.66);
+		atm.deposit("MrTheiss",64033.55);
+		atm.withdraw("MrTheiss", 443964.46);
+		atm.checkBalance("Bezos");
+		atm.checkBalance("Winfrey");
+		atm.checkBalance("YoMama");
+		atm.checkBalance("MrTheiss");
+	}
+
+}
--- src/SimpleATM.java
+++ src/SimpleATM.java
@@ -1,16 +1,43 @@
 import java.util.*;
-
 public class SimpleATM
 {
 	private HashMap <String, Double> map = new HashMap <> ();
-	
+	public double checkBalance(String AccountID)
+	{
+		if(map.containsKey(AccountID)) {
+			return map.get(AccountID);
+		}
+		else
+		{
+			throw new IndexOutOfBoundsException("No such ID exists");
+		}
+	}
+	public void withdraw(String id,double amount)
+	{
+		if (map.containsKey(id))
+		{
+			if(map.get(id)<amount)
+			{
+				System.out.println("please don't steal money from us");
+			}
+			else {
+			double oldValue = map.remove(id);
+			double newValue = oldValue - amount;
+			map.put(id, newValue);
+			}
+		}
+		else
+		{
+			throw new IndexOutOfBoundsException("no such id exists");
+		}
+	}
 	public void deposit (String id, double amount)
 	{
 		if (amount > 0)
 		{
 			if (map.containsKey(id))
 			{
-				double oldValue = map.get(id);
+				double oldValue = map.remove(id);
 				double newValue = oldValue + amount;
 				map.put(id, newValue);
 			}
